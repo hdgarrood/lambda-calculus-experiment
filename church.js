4896d66fef8d17d37b9df40a8feaa2453f7b7976
Harry Garrood <harry@garrood.me>
2015-02-16 01:27:48 +0000

    aww yeah

--- church.js
+++ church.js
@@ -29,47 +29,49 @@ var fifteen = mult(three)(five)
 var one_hundred = mult(ten)(ten)
 
 // booleans
-var Qtrue  = (a) => (b) => a
-var Qfalse = (a) => (b) => b
+// The intended use here is that when using Qif, you supply 0-arity functions
+// that simply return the values you wanted. We can't just put the values in,
+// because then both branches are forced, and this means recursive functions
+// don't work.
+var Qtrue  = (a) => (b) => a()
+var Qfalse = (a) => (b) => b()
 var Qif    = (b) => b
 
-var isZero = (n) => n(Qconst(Qfalse))(Qtrue)
+var isZero = (n) =>
+  n(Qconst(Qfalse))(Qtrue)
 
 // decrement is a little harder
 var tupleify = (f) => (t) =>
   Qif(snd(t))(
-      tuple(f(fst(t)))(Qtrue))(
-      tuple(fst(t))(Qtrue))
+    () => tuple(f(fst(t)))(Qtrue))(
+    () => tuple(fst(t))(Qtrue))
 
 var dec = (n) => (f) => (x) =>
   fst(n(tupleify(f))(tuple(x)(Qfalse)))
 
 // more arithmetic
-var sub_recur = (recur) => (n) => (m) =>
-  toBool(isZero(n))
-    ? m
-    : toBool(isZero(m))
-      ? n
-      : recur(dec(n))(dec(m))
-  // Qif(isZero(n))(
-  //     m)(Qif(isZero(m))(
-  //         n)(recur(dec(m))(dec(n))))
-var sub = Y(sub_recur)
-
-var almostFactorial = (recur) => (n) =>
+var subRecur = (recur) => (n) => (m) =>
   Qif(isZero(n))(
-      one)(
-      mult(n)(recur(dec(n))))
+    () => m)(
+    () => Qif(isZero(m))(
+      () => n)(
+      () => recur(dec(m))(dec(n))))
+var sub = Y(subRecur)
 
-var factorial = Y(almostFactorial)
+var factorialRecur = (recur) => (n) =>
+  Qif(isZero(n))(
+    () => one)(
+    () => mult(n)(recur(dec(n))))
 
-// var div = (n) => (m) => (f) => (x) =>
+var factorial = Y(factorialRecur)
+
+var div = (n) => (m) =>
   
 
 // var mod = (n) => (m) => (f) => (x) =>
   
 
 var toNum  = (n) => n((x) => x + 1)(0)
-var toBool = (b) => Qif(b)(true)(false)
+var toBool = (b) => Qif(b)(() => true)(() => false)
 
 //document.write(toNum(factorial(five)))
